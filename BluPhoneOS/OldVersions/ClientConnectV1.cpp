966bf53e738302b53623d6f564a36e74a8e9c47e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-03 17:08:43 -0800

    ClientConnectV1.cpp
    An archive of the first version of the client connection tool script for BluPhoneOS.

---
+++ BluPhoneOS/OldVersions/ClientConnectV1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+// Start of script
+
+int main()
+{
+    std::cout << "Preparing to connect to a BluPhone client\n";
+}
+int clientList()
+{
+
+}
+int scan()
+{
+  // Scans for clients, lets the user choose which client to connect to (clients can be password protected, new upcoming feature)
+  std:cout << "Select a BluPhone client:\n";
+  clientList()
+}
+// End of script
+/*
+File info
+File version: 1 (Monday, November 3rd 2020 at 5:08 pm)
+Line count (including blank lines and compiler line): 27
+File type: C++ Script file (*.cpp)
+*/
